318f64f5288f236d8ab04d5c7c28e9c6bf348a76
pensadotech <armando@pensadotech.com>
2018-09-30 21:29:44 -0700

    Create switch structure to process commands

---
+++ liri.js
@@ -0,0 +1,42 @@
+//
+// LIRI is a _Language_ Interpretation and Recognition Interface
+//
+
+
+//set any environment variables
+require("dotenv").config();
+// require modules
+const keys = require('./keys.js');
+const Spotify = require('node-spotify-api');
+const request = require('request');
+const moment = require('moment');
+
+// access keys information
+var spotify = new Spotify(keys.spotify);
+var cmd = process.argv[2];
+// concatenate all arguments to build search topic
+var searchTopic = process.argv.slice(3).join('+');;
+
+// Process commands
+switch (cmd) {
+  case 'concert-this':
+    // node liri.js concert-this <artist/band name here>
+    console.log("concert-this");
+    console.log(searchTopic);
+    break;
+  case 'spotify-this-song':
+    // node liri.js spotify-this-song '<song name here>'
+    console.log("spotify-this-song");
+    break;
+  case 'movie-this':
+    // node liri.js movie-this '<movie name here>'
+    console.log("movie-this");
+    break;
+  case 'do-what-it-says':
+    // node liri.js do-what-it-says
+    console.log("do-what-it-says");
+    break;
+  default:
+    console.log("invalid command");
+    break;
+}
