d279a537ba0963c993d91c2eb9d7c653af76c842
pensadotech <armando@pensadotech.com>
2018-10-01 21:12:09 -0700

    add concert-this, movie-this, and do-what-it-says

--- liri.js
+++ liri.js
@@ -2,51 +2,101 @@
 // LIRI is a _Language_ Interpretation and Recognition Interface
 //
 
+// require modules ........................................................
 //set any environment variables
 require("dotenv").config();
-// require modules
+// prepare modules
 const keys = require('./keys.js');
 const Spotify = require('node-spotify-api');
 const request = require('request');
 const moment = require('moment');
+const fs = require("fs");
 
-
-var cmd = process.argv[2];
-// concatenate all arguments to build search topic
-var searchTopic = process.argv.slice(3).join('+');;
-
-// Process commands
-switch (cmd) {
-  case 'concert-this':
-    // node liri.js concert-this <artist/band name here>
-    console.log("concert-this");
-    console.log(searchTopic);
-    break;
-  case 'spotify-this-song':
-    // node liri.js spotify-this-song '<song name here>'
-    console.log("spotify-this-song");
-    console.log(searchTopic);
-    spotifySong(searchTopic);
-    break;
-  case 'movie-this':
-    // node liri.js movie-this '<movie name here>'
-    console.log("movie-this");
-    console.log(searchTopic);
-    break;
-  case 'do-what-it-says':
-    // node liri.js do-what-it-says
-    console.log("do-what-it-says");
-    console.log(searchTopic);
-    break;
-  default:
-    console.log("invalid command");
-    break;
+// Main logic ..............................................................
+if (process.argv.length < 3) {
+  console.log("missing argument: command (e.g. node liri movie-this ....) ")
+} else {
+  var actionCmd = process.argv[2];
+  var searchTopic = '';
+  // concatenate all arguments to build search topic
+  if (process.argv.length >= 4) {
+    searchTopic = process.argv.slice(3).join('+');
+  }
+  console.log("actionCmd: " + actionCmd);
+  console.log("searchTopic: " + searchTopic);
+  // Process commands
+  takeAction(actionCmd, searchTopic);
 }
 
+// Functions ..............................................................
+function takeAction(actionCmd, searchTopic) {
+  // Process commands
+  switch (actionCmd) {
+    case 'concert-this':
+      // node liri.js concert-this <artist/band name here>
+      // execute command
+      concertThis(searchTopic);
+      break;
+    case 'spotify-this-song':
+      // node liri.js spotify-this-song '<song name here>'
+      // execute command
+      spotifySong(searchTopic.trim());
+      break;
+    case 'movie-this':
+      // node liri.js movie-this '<movie name here>'
+      // execute command
+      movieThis(searchTopic.trim());
+      break;
+    case 'do-what-it-says':
+      // node liri.js do-what-it-says
+      // execute acction 
+      doWhatItSay();
+      break;
+    default:
+      console.log("invalid command");
+      break;
+  }
+}
 
+function concertThis(searchTopic) {
+
+  // * Name of the venue
+  // * Venue location
+  // * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+  // Look for the band
+  if (searchTopic === '') {
+    console.log("Invalid band entry!");
+  } else {
+    // example: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    let url = 'https://rest.bandsintown.com/artists/';
+    url += searchTopic;
+    url += '/events?app_id=codingbootcamp';
+
+    request(url, function (err, response, body) {
+      if (err) {
+        console.log(err);
+      } else {
+        // prepare resonse
+        let data = JSON.parse(body);
+        let eventNum = 0;
+        console.log("Band: " + searchTopic)
+        data.forEach(element => {
+          console.log('Event #' + eventNum + '------------------------');
+          console.log('Name of the venue: ' + element.venue.name);
+          console.log('Venue location: ' + element.venue.city + ', ' + element.venue.country);
+          console.log('Date of the Event: ' + moment(element.datetime).format("MM/DD/YYYY HH:MM"));
+          eventNum++;
+        });
+      }
+    })
+  }
+
+
+}
 
 function spotifySong(searchTopic) {
-  // Default song, if nothing was provided
+  // Default search topic, if nothing was provided
   if (searchTopic === '') searchTopic = 'The Sign';
   // access keys information
   var spotify = new Spotify(keys.spotify);
@@ -57,12 +107,12 @@ function spotifySong(searchTopic) {
       query: searchTopic
     })
     .then(function (response) {
-      
+
       // * Artist(s)
       // * The song's name
       // * A preview link of the song from Spotify
       // * The album that the song is from
-      
+
       console.log(response);
 
       // var tracks = response.tracks;
@@ -83,4 +133,81 @@ function spotifySong(searchTopic) {
       console.log(err);
     });
 
+}
+
+function movieThis(searchTopic) {
+  // Default search topic, if nothing was provided
+  if (searchTopic === '') searchTopic = 'Mr. Nobody';
+
+  // * Title of the movie.
+  // * Year the movie came out.
+  // * IMDB Rating of the movie.
+  // * Rotten Tomatoes Rating of the movie.
+  // * Country where the movie was produced.
+  // * Language of the movie.
+  // * Plot of the movie.
+  // * Actors in the movie.
+
+  // Look for the movie
+  // example: http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy
+  let url = 'http://www.omdbapi.com/';
+  url += '?t=' + searchTopic;
+  url += '&y=&plot=short';
+  url += '&apikey=trilogy';
+
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      // prepare resonse
+      let data = JSON.parse(body);
+      // console.log(data);
+      if (data.Title === undefined) {
+        console.log('Movie not found!');
+      } else {
+        // get Rotten Tomatoes rating
+        let rottenTommatoes = '';
+        data.Ratings.forEach(element => {
+          if (element.Source === 'Rotten Tomatoes') {
+            rottenTommatoes = element.Value;
+          }
+        });
+        // display teh required info in console
+        console.log('Title: ' + data.Title);
+        console.log('Year: ' + data.Year);
+        console.log('imdbRating: ' + data.imdbRating);
+        console.log('Rotten Tomatoes: ' + rottenTommatoes);
+        console.log('Country: ' + data.Country);
+        console.log('Language: ' + data.Language);
+        console.log('Plot: ' + data.Plot);
+        console.log('Actors: ' + data.Actors);
+      }
+    }
+  })
+}
+
+function doWhatItSay() {
+  // read file and take action 
+  fs.readFile("random.txt", "utf8", function (err, data) {
+    if (err) {
+      return console.log(err)
+    } else {
+      let dataArr = data.split(",");
+      if (dataArr.legth === 2) {
+        // First element is the command
+        let actionCmd = dataArr[0];
+        // search topic
+        let searchTopic = dataArr[1].replace(/"/g, '');
+        searchTopic = searchTopic.slice(0).join('+');
+        // Process commands
+        if (actionCmd !== 'do-what-it-says') {
+          takeAction(actionCmd, searchTopic);
+        } else {
+          console.log('do-what-it-says is invalid, as it can cause an infinite loop.')
+        }
+      } else  {
+        console.log('Invalid file content!')
+      }
+    }
+  })
 }
